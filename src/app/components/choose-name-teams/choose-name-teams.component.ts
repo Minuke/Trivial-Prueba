1086b78641c0e471ea52ebbfb6d9430d942ff51e
Minuke <nakamadeath@gmail.com>
2023-12-20 15:47:55 +0100

    basic form for writing team names

--- src/app/components/choose-name-teams/choose-name-teams.component.ts
+++ src/app/components/choose-name-teams/choose-name-teams.component.ts
@@ -1,14 +1,30 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-choose-name-teams',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './choose-name-teams.component.html',
   styleUrl: './choose-name-teams.component.scss'
 })
 export class ChooseNameTeamsComponent {
 
   @Input() teams!: number;
+
+  constructor(private fb: FormBuilder) { }
+
+  teamForm = this.fb.group({
+    name: ['', Validators.required],
+  });
+
+  get name() {
+    return this.teamForm.get('name');
+  }
+
+  onSubmit() {
+    console.log(this.teamForm.value);
+  }
 
 }
