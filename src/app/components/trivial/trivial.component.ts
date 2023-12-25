9482b5b5fd89970bae09e54d8b9862233ab6ae0a
Minuke <nakamadeath@gmail.com>
2023-12-25 13:29:50 +0100

    saved score in browser's localstorage

--- src/app/components/trivial/trivial.component.ts
+++ src/app/components/trivial/trivial.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component, EventEmitter, Input, Output, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { GameService } from 'app/services/game.service';
 import { Team } from 'app/shared/models/interfaces/team.interface';
 import { Trivial } from 'app/shared/models/interfaces/trivial.interface';
 
@@ -24,6 +25,7 @@ export class TrivialComponent {
   public totalCorrectAnswerSelected:number = 0;
   public nextQuestion:boolean = false;
 
+  private gameService:GameService = inject(GameService);
   private router:Router = inject(Router);
 
   selectAnswer(selectedAnswer:string, correct:boolean):void {
@@ -36,6 +38,7 @@ export class TrivialComponent {
           answer.selected = true;
           if(correct) {
             teamActually.score += 1;
+            this.gameService.setDatos(this.teams);
             this.totalCorrectAnswerSelected += 1;
             this.nextQuestion = this.questionIsFinished(this.questionData.totalCorrectAnswers);
           }
