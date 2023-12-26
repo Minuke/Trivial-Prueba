a7318eaf989e19645bf1c48226a6e980f7ce0ae0
Minuke <nakamadeath@gmail.com>
2023-12-27 00:33:36 +0100

    navigation from home to game prohibited when no equipment data is defined

--- src/app/routes/game.routes.ts
+++ src/app/routes/game.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
+import { gameGuard } from "app/guards/game.guard";
 import { GamePageComponent } from "app/pages/game-page/game-page.component";
 
 export const gameRoutes: Routes = [
   {
     path:"",
-    component: GamePageComponent
+    component: GamePageComponent,
+    canActivate: [gameGuard]
   }
 ]
