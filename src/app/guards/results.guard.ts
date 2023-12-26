42d23a3b06ee22d3241c9d1303f15332244f48fa
Minuke <nakamadeath@gmail.com>
2023-12-27 00:43:15 +0100

    navigation from home to results prohibited when no device data is defined

---
+++ src/app/guards/results.guard.ts
@@ -0,0 +1,15 @@
+
+import { CanActivateFn, Router } from '@angular/router';
+import { GameService } from 'app/services/game.service';
+
+export const resultsGuard: CanActivateFn = (route, state) => {
+  const dataService = new GameService();
+  const router = new Router();
+  if (dataService.getDatos().length > 0) {
+    return true;
+  } else {
+    router.navigate(['/home']);
+    return false;
+  }
+};
+
--- src/app/routes/results.routes.ts
+++ src/app/routes/results.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from "@angular/router";
+import { resultsGuard } from "app/guards/results.guard";
 import { ResultsPageComponent } from "app/pages/results-page/results-page.component";
 
 export const resultsRoutes: Routes = [
   {
     path:"",
-    component: ResultsPageComponent
+    component: ResultsPageComponent,
+    canActivate: [resultsGuard]
   }
 ]
